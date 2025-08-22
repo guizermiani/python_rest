70194991cd9d1afba17495c8282afcf9e43128c2
guizermiani <guilherme.zermianitec@gmail.com>
2025-08-21 21:50:22 -0300

    Criptografia + Outras Alterações
    Adição de Criptografia na Senha, Salvar Usuário, Listar Usuário, etc.

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from bd.produto import listar_produto_bd, consultar_produto_por_id_bd, inserir_p
 
 from routes.categoria_routes import categoria_bp
 from routes.produto_routes import produto_bp
+from routes.usuario_routes import usuario_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(categoria_bp)
     app.register_blueprint(produto_bp)
+    app.register_blueprint(usuario_bp)
     return app
 
 
