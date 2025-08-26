d46be55debc4661dba8e582021cfb8f7fb5a4d6b
guizermiani <guilherme.zermianitec@gmail.com>
2025-08-26 13:27:03 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -2,8 +2,8 @@ import psycopg2
 
 def conecta_db():
    con =  psycopg2.connect(host="127.0.0.1",
-                    database="curso",
-                    user='postgres',
-                    password="postgres",
+                    database="postgres",
+                    user="postgres",
+                    password="10042009",
                     port=5432)
    return con
